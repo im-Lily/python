afccef00ba9dfccc414fc938538fd17cc6bef8cf
im-Lily <67992684+im-Lily@users.noreply.github.com>
2021-05-21 23:49:35 +0900

    add basic_1 file about format, slicing

---
+++ basic_1.py
@@ -0,0 +1,12 @@
+### format ###
+
+'My name is %s' % 'lily'
+'My name is {}'.format('lily')
+ 
+ '{} * {} = {}'.format(2,3,6)
+ '{2} * {1} = {0}'.format(2,3,6)
+
+ ### slicing ###
+str = 'python'
+str[2:6]
+str[2:5]
